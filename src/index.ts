a4b4207a0a0dac53568d378ba9acf55f556ee80e
Eray Alkış <erayalkis@hotmail.com>
2023-07-29 12:37:25 +0300

    Use logstats for debug

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import color from "./helpers/color.ts";
 import statsRouter from "./routers/stats.ts";
 import { consoleLogger } from "./helpers/middleware.ts";
+import { logStats } from "./helpers/os.ts";
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,10 @@ app.use(consoleLogger);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
+
+setInterval(() => {
+  logStats();
+}, 1000);
 
 app.listen(port, () => {
   console.log(
