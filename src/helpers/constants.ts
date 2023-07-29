0ff54de42161cc93052ef391b2633f67d5d87291
Eray Alkış <erayalkis@hotmail.com>
2023-07-29 13:12:06 +0300

    Create CONSOLELOGSDIR constant

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -2,3 +2,4 @@ import { PathLike } from "fs";
 
 export const LOGSDIR: PathLike = "./logs";
 export const USAGELOGSDIR = LOGSDIR + "/usage.txt";
+export const CONSOLELOGSDIR = LOGSDIR + "/console.txt";
