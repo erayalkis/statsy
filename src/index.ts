9588b008a31204e604674c5199598dab3f25a615
Eray Alkış <erayalkis@hotmail.com>
2023-07-29 12:55:16 +0300

    Use new functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,16 @@ import color from "./helpers/color.ts";
 import statsRouter from "./routers/stats.ts";
 import { consoleLogger } from "./helpers/middleware.ts";
 import { logStats } from "./helpers/os.ts";
+import { ensureLogFilesExists } from "./helpers/startup.ts";
 
 const app = express();
 const port = 3000;
+
+ensureLogFilesExists();
+
+setInterval(() => {
+  logStats(true, true);
+}, 1000);
 
 app.use("/stats", statsRouter);
 app.use(consoleLogger);
@@ -13,10 +20,6 @@ app.use(consoleLogger);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
-
-setInterval(() => {
-  logStats();
-}, 1000);
 
 app.listen(port, () => {
   console.log(
