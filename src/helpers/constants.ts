9115c0e89dcff15dc927df35d70859840d6777fb
Eray Alkış <erayalkis@hotmail.com>
2023-08-01 00:25:00 +0300

    Add new ENV args to constants.ts

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,3 +3,11 @@ import { PathLike } from "fs";
 export const LOGSDIR: PathLike = "./logs";
 export const USAGELOGSDIR = LOGSDIR + "/usage.txt";
 export const CONSOLELOGSDIR = LOGSDIR + "/console.txt";
+export const SHOULD_LOG_USAGE_TO_FILE =
+  process.env["LOG_USAGE_TO_FILE"] !== undefined;
+export const SHOULD_LOG_USAGE_TO_CONSOLE =
+  process.env["LOG_USAGE_TO_CONSOLE"] !== undefined;
+export const SHOULD_LOG_REQUESTS_TO_FILE =
+  process.env["LOG_REQUESTS_TO_FILE"] !== undefined;
+export const SHOULD_LOG_REQUESTS_TO_CONSOLE =
+  process.env["LOG_REQUESTS_TO_CONSOLE"] !== undefined;
