e4fe4d71d5b91b1f84ac0e532e4956364974f75a
Eray Alkış <erayalkis@hotmail.com>
2023-08-01 00:53:57 +0300

    Activate usage logger only if one of the args is true

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,21 @@ import statsRouter from "./routers/stats.ts";
 import { consoleLogger } from "./helpers/middleware.ts";
 import { logStats } from "./helpers/os.ts";
 import { ensureLogFilesExists } from "./helpers/startup.ts";
+import {
+  SHOULD_LOG_USAGE_TO_CONSOLE,
+  SHOULD_LOG_USAGE_TO_FILE,
+} from "./helpers/constants.ts";
 
 const app = express();
 const port = 3000;
 
 ensureLogFilesExists();
 
-setInterval(() => {
-  logStats(false, false);
-}, 1000);
+if (SHOULD_LOG_USAGE_TO_CONSOLE || SHOULD_LOG_USAGE_TO_FILE) {
+  setInterval(() => {
+    logStats(SHOULD_LOG_USAGE_TO_CONSOLE, SHOULD_LOG_USAGE_TO_FILE);
+  }, 1000);
+}
 
 app.use("/stats", statsRouter);
 app.use(consoleLogger);
