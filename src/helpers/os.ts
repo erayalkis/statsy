291c0dcbc20f941f294450fe44b32e5fd27d1380
Eray Alkış <lolpo12345@outlook.com>
2024-03-30 17:42:36 +0300

    Add CPU temperature calculations to statsy

--- src/helpers/os.ts
+++ src/helpers/os.ts
@@ -1,22 +1,26 @@
 import os from "os";
 // @ts-ignore
-import console from "./console.js";
+import consoleAlt from "./console.js";
 import { writeFile } from "fs";
 // @ts-ignore
 import { USAGELOGSDIR } from "./constants.js";
-import { cpuTemperature } from "systeminformation"
+import { cpuTemperature } from "systeminformation";
 
 const stats = {
-  memFree: () => os.freemem(),
-  memTotal: () => os.totalmem(),
+  memFree: () => Math.round(os.freemem() / 1000000),
+  memTotal: () => Math.round(os.totalmem() / 1000000),
   cpus: () => os.cpus(),
   loadAvg: () => os.loadavg(),
   uptime: () => os.uptime(),
+  temp: async () =>
+    cpuTemperature().then((tmp) => {
+      return `MAIN: ${tmp.main} MAX: ${tmp.max}`;
+    }),
 };
 
-export const logStats = (logToConsole: boolean, logToFile: boolean) => {
-  const logText = `[USAGE STATISTICS]-[${new Date().toISOString()}] CPU: ${stats.loadAvg()} MEMFREE: ${stats.memFree()} MEMTOTAL: ${stats.memTotal()} UPTIME: ${stats.uptime()} `;
-  if (logToConsole) console.debugBright(logText);
+export const logStats = async (logToConsole: boolean, logToFile: boolean) => {
+  const logText = `[USAGE STATISTICS]-[${new Date().toISOString()}] CPU: ${stats.loadAvg()} TEMP: [${await stats.temp()}] MEMFREE: ${stats.memFree()} MEMTOTAL: ${stats.memTotal()} UPTIME: ${stats.uptime()} `;
+  if (logToConsole) consoleAlt.debugBright(logText);
 
   if (logToFile) {
     writeFile(USAGELOGSDIR, logText + "\n", { flag: "a" }, () => {});
